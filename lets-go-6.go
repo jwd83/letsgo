cafb81e65c7ddc6c667e11950ef3d7d8b6e360dc
Jared De Blander <jared@deblander.org>
2016-10-06 01:47:04 -0400

    Starting lets-go-6.go

---
+++ lets-go-6.go
@@ -0,0 +1,27 @@
+package main
+/*
+
+Go Language Examples. These files are intended to be read in the order of their
+filenames.
+
+   ooooo  oooo     oooo  ooooooooo
+    888    88   88  88    888    88o
+    888     88 888 88     888    888
+    888      888 888      888    888
+    888       8   8      o888ooo88
+ 8o888
+
+Copyright (C) Jared De Blander
+ _       _                 _            _   _
+(_)     | |               | |          | | (_)
+ _ _ __ | |_ _ __ ___   __| |_   _  ___| |_ _  ___  _ __
+| | '_ \| __| '__/ _ \ / _` | | | |/ __| __| |/ _ \| '_ \
+| | | | | |_| | | (_) | (_| | |_| | (__| |_| | (_) | | | |
+|_|_| |_|\__|_|  \___/ \__,_|\__,_|\___|\__|_|\___/|_| |_|
+
+*/
+import "fmt"
+
+func main() {
+
+}
